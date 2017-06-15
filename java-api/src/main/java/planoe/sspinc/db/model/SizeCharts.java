7953e046fa2dfe01686403b973ed87dbae2e7d9f
Philippe Lanoë <philippe.lanoe@gmail.com>
2017-06-15 19:08:47 +0200

    Switched to List in DB model for more consistency

--- java-api/src/main/java/planoe/sspinc/db/model/SizeCharts.java
+++ java-api/src/main/java/planoe/sspinc/db/model/SizeCharts.java
@@ -5,19 +5,19 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.google.auto.value.AutoValue;
 
-import java.util.Set;
+import java.util.List;
 
 @AutoValue
 @JsonDeserialize(builder = AutoValue_SizeCharts.Builder.class)
 public abstract class SizeCharts {
 
     @JsonProperty
-    public abstract Set<Brand> getBrands();
+    public abstract List<Brand> getBrands();
 
     @AutoValue.Builder
     abstract static class Builder {
         @JsonProperty("brands")
-        abstract Builder setBrands(Set<Brand> brands);
+        abstract Builder setBrands(List<Brand> brands);
         abstract SizeCharts build();
     }
 
