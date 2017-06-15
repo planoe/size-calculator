b684b82fef0f40c3369a6820b5223ce82c9b3eaa
Philippe Lanoë <philippe.lanoe@gmail.com>
2017-06-15 15:23:51 +0200

    Added autoconfig of the Guice bundle - no need to register manually resources

--- java-api/src/main/java/planoe/sspinc/SizeCalculatorApp.java
+++ java-api/src/main/java/planoe/sspinc/SizeCalculatorApp.java
@@ -3,13 +3,9 @@ package planoe.sspinc;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.hubspot.dropwizard.guice.GuiceBundle;
 import io.dropwizard.Application;
-import io.dropwizard.Configuration;
 import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import planoe.sspinc.resources.BrandResource;
-import planoe.sspinc.resources.CategoryResource;
-import planoe.sspinc.resources.SizePredictionResource;
 
 /**
  * Created by philippe on 10/06/17.
@@ -24,6 +20,7 @@ public class SizeCalculatorApp extends Application<SizeCalculatorConfiguration> 
     public void initialize(Bootstrap<SizeCalculatorConfiguration> bootstrap) {
         GuiceBundle<SizeCalculatorConfiguration> guiceBundle = GuiceBundle.<SizeCalculatorConfiguration>newBuilder()
                 .addModule(new SizeCalculatorModule())
+                .enableAutoConfig(getClass().getPackage().getName())
                 .setConfigClass(SizeCalculatorConfiguration.class)
                 .build();
         bootstrap.addBundle(guiceBundle);
@@ -34,10 +31,6 @@ public class SizeCalculatorApp extends Application<SizeCalculatorConfiguration> 
 
     public void run(SizeCalculatorConfiguration configuration, Environment environment) throws Exception {
         environment.getObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
-
-        environment.jersey().register(BrandResource.class);
-        environment.jersey().register(CategoryResource.class);
-        environment.jersey().register(SizePredictionResource.class);
     }
 
 
